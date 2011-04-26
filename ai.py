76fd36ef5dc3161c899807d85e8a32581d536bdc
Teifion <sarkalian@gmail.com>
2011-04-26 12:25:36 +0100

    AI can now make simple choices

---
+++ ai.py
@@ -0,0 +1,29 @@
+import time
+
+class Game_ai (object):
+	def __init__(self, game):
+		super(Game_ai, self).__init__()
+		self.game = game
+		self.move = (-1,-1)
+	
+	def make_move(self, excep=True):
+		changes = {}
+		
+		for x in range(0,8):
+			for y in range(0,8):
+				if self.game.board[x][y] == 0:
+					c = self.game.place_piece(x, y, live_mode=False)
+					if c > 0:
+						changes[(x,y)] = c
+		
+		max_key, max_val = (-1,-1), 0
+		
+		for k, v in changes.items():
+			if v > max_val:
+				max_key = k
+		
+		self.move = max_key
+	
+	def perform_move(self):
+		time.sleep(1)
+		self.game._perform_move(self.move)
